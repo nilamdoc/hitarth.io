672646b4d478212a063599f8ce959cffa72ca66e
Nilam Doctor <nilamdoc@gmail.com>
2020-12-01 02:06:33 +0530

    upload

---
+++ config/bootstrap/connections.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace app\config\bootstrap;
+
+use lithium\data\Connections;
+
+ Connections::add('default', array(
+ 	'type' => CONNECTION_TYPE,
+ 	'host' => array(CONNECTION,
+		),
+//	'replicaSet' => true,
+ 	'database' => CONNECTION_DB,
+	'login' => CONNECTION_USER,
+	'password' => CONNECTION_PASS,	
+	//'classes' => array('server' => 'Mongo')
+//	'setSlaveOkay' => true,
+//	'readPreference' => Mongo::RP_NEAREST	
+ ));
+
